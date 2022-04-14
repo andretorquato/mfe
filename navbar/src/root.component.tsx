6d3ee59c3dbe348f13c2ae931de36291e25d3909
André Torquato <andretorquato@alu.ufc.br>
2022-04-13 23:49:28 -0300

    display navbar in entire project

--- navbar/src/root.component.tsx
+++ navbar/src/root.component.tsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
+import BasicTabs from './navbar';
 
 import './styles.css';
 
 export default function Root(props) {
-  const [count, updateCount] = useState(0);
-  console.log('Root props:', props);
-  function incrementCount() {
-    updateCount(count + 1);
-  }
-   return (
-      <div className="container">
-        <h1>Navbar</h1>
-        <p>{props.name}</p>
-        <button onClick={incrementCount}>Increment</button>
-        <p>{count}</p>
-      </div>
-    );
+   return <BasicTabs />
 }
